88f06671abb1c7f731b0deedcc154efedf424882
nguyen22-es <112994817+nguyen22-es@users.noreply.github.com>
2023-06-03 11:22:32 +0700

    maytinh

---
+++ CuaHangCongNghe/Controllers/DanhmucController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication4.Controllers;
+
+namespace CuaHangCongNghe.Controllers
+{
+    public class DanhmucController : Controller
+    {
+        private readonly ILogger<DanhmucController> _logger;
+
+        public DanhmucController(ILogger<DanhmucController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult maytinh()
+        {
+            return View();
+        }
+    }
+}
