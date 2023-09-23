d9c2f936ee74aabb4597454a1b392e9f51bda24a
nguyen22-es <112994817+nguyen22-es@users.noreply.github.com>
2023-09-23 09:12:18 +0700

    Register

--- CuaHangCongNghe/Controllers/admincontroller.cs
+++ CuaHangCongNghe/Controllers/admincontroller.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Shop.Extensions;
-using System.Security.Claims;
+
 
 namespace CuaHangCongNghe.Controllers
 {
